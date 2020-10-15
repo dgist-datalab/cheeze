f1f7fda0c7b38c464ef832947a9fe70a09476e58
jhk5361 <jhk5361@gmail.com>
2020-10-15 21:50:45 +0900

    add IS_IN_VM

--- cheeze.h
+++ cheeze.h
@@ -26,9 +26,15 @@
 #define REQS_OFF (SEQ_OFF + SEQ_SIZE)
 #define REQS_SIZE (CHEEZE_QUEUE_SIZE * sizeof(struct cheeze_req_user))
 
+#ifdef IS_IN_VM
 #define SHM_ADDR0 0x800000000
 #define SHM_ADDR1 0x840000000
 #define SHM_ADDR2 0x880000000
+#else
+#define SHM_ADDR0 0x3f40000000
+#define SHM_ADDR1 0x3f00000000
+#define SHM_ADDR2 0x3ec0000000
+#endif
 
 #define SKIP INT_MIN
 
